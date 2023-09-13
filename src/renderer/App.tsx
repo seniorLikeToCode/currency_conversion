8fc8878fb8dd156b4f8e751f241ffbb78634a52b
seniorliketocode <abhishek234543@gmail.com>
2023-09-13 19:37:37 +0530

    api added

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,93 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
-function Hello() {
+async function fetchCountries() {
+  const response = await fetch(
+    'https://v6.exchangerate-api.com/v6/key-here/codes'
+  );
+  const data = await response.json();
+  return data.supported_codes;
+}
+
+async function convert(from: string, to: string, amount: number) {
+  const response = await fetch(
+    `https://v6.exchangerate-api.com/v6/key-here/pair/${from}/${to}/${amount}`
+  );
+  const data = await response.json();
+  console.log(data);
+  return data.conversion_result;
+}
+
+function Home() {
+  const [countries, setCountries] = useState([]);
+  const [from, setFrom] = useState('USD');
+  const [to, setTo] = useState('INR');
+  const [amount, setAmount] = useState(1);
+  const [result, setResult] = useState(0);
+
+  useEffect(() => {
+    // fetchCountries().then((data) => {
+    //   setCountries(data);
+    // });
+    // convert(from, to, amount).then((data) => {
+    //   setResult(data);
+    // });
+  }, []);
+
+  const handleFromChange = (e: any) => {
+    setFrom(e.target.value);
+  };
+
+  const handleToChange = (e: any) => {
+    setTo(e.target.value);
+  };
+
+  const handleAmountChange = (e: any) => {
+    setAmount(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    convert(from, to, amount).then((data) => {
+      setResult(data);
+    });
+  };
+
   return (
-    <div className='bg-black'>
-      <div className="Hello bg-black">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1 className='text-black'>Ele-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+    <div className="h-screen bg-slate-100">
+      <h1 className="text-center text-2xl">Simple Currency Converter</h1>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-lg">Exchange Rate</h1>
+        <p className="text-5xl ">{result}</p>
+
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col">
+            <label>Amount</label>
+            <input type="number" name="amount"></input>
+          </div>
+          <div>
+            <div>
+              <label>From</label>
+              <select name="from">
+                {countries.map((country: any) => (
+                  <option value={country[0]}>{country[1]}</option>
+                ))}
+              </select>
+            </div>
+
+            <div>
+              <label>To</label>
+              <select name="to">
+                {countries.map((country: any) => (
+                  <option value={country[0]}>{country[1]}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <button type="submit">Convert</button>
+        </form>
       </div>
     </div>
   );
@@ -43,7 +97,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
